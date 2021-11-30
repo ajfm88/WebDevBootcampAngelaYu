d60df6a042c12f1cf01e3abe30835a1f8a8eff60
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-11-29 21:17:05 -0800

    Nov. 29th, 2021

--- 15_TheSimonGame/game.js
+++ 15_TheSimonGame/game.js
@@ -1,19 +1,59 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
+var userClickedPattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
 function nextSequence() {
 
-  //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
-
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
   var randomChosenColour = buttonColours[randomNumber];
-
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
   gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }

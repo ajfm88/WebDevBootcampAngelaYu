3aa7beff59066f7d8fe62ae4dda43943ded12b1d
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-07 12:03:11 -0800

    Dec. 07, 2021

--- 19_TodoListV1/app.js
+++ 19_TodoListV1/app.js
@@ -4,14 +4,43 @@ const app = express();
 app.use(express.urlencoded({ extended : true}));
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("Yay, it's the weekend!");
-    } else {
-        res.send("Boo! I have to work!");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log('Error: current day is equal to: ' + currentDay);
     }
+
+    res.render("list", {
+        kindOfDay: day
+    });
+    
 });
 
 app.listen(3000, function(){

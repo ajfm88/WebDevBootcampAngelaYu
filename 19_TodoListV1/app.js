332954a50faa2d9ba70312de1ddff19c3fb5a463
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-09 00:03:44 -0800

    Dec. 08, 2021

--- 19_TodoListV1/app.js
+++ 19_TodoListV1/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const date = require(__dirname + "/date.js");
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static('public'));
@@ -11,27 +13,39 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
 
-    var today = new Date();
+    const day = date.getDate();
 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    var day = today.toLocaleDateString("en-US", options); 
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
 
-    items.push(item);
-    
-    res.redirect('/');
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+
 });
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 app.listen(3000, function(){
     console.log('Server started on port 3000');

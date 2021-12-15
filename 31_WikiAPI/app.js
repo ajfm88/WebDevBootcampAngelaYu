762ee6ac2b5e8cba1f918f3b97a91c753f74774a
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-15 14:45:03 -0800

    Dec. 15, 2021

--- 31_WikiAPI/app.js
+++ 31_WikiAPI/app.js
@@ -89,6 +89,21 @@ app.route("/articles/:articleTitle")
             }
         }
     );
+})
+
+.patch(function(req, res) {
+
+    Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article.");
+            } else {
+                res.send(err);
+            }
+        }
+    );
 });
 
 app.listen(3000, function() {

b881b3efafbd696c6ddfd2e55e74c3f0af166815
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-25 12:36:40 -0800

    Login Form Update

--- 37_LoginForm/src/components/App.jsx
+++ 37_LoginForm/src/components/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-var isLoggedIn = false;
-
-const currentTime = new Date(2019, 11, 1, 9).getHours();
-console.log(currentTime);
+var userIsRegistered = true;
 
 function App() {
   return (
     <div className="container">
-      {/*Ternary Operator*/}
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {/*AND Operator*/}
-      {currentTime > 12 && <h1>Why are you still working?</h1>}
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
